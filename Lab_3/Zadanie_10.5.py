1896d9bb00767b78ff26a65541df71ad0ace8e4e
Karolkzsp5 <kondrackikarol688@gmail.com>
2025-04-01 18:52:47 +0200

    Zadania lab_3

---
+++ Lab_3/Zadanie_10.5.py
@@ -0,0 +1,3 @@
+liczby = list(range(1, 21))
+nieparzyste = [x for x in liczby if x % 2 != 0]
+print(nieparzyste)
---
+++ Lab_3/Zadanie_10.6.py
@@ -0,0 +1,3 @@
+import numpy as np
+macierz = np.array([[(i+1)*(j+1) for j in range(4)] for i in range(4)])
+print(macierz)
