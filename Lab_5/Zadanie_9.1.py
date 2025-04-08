a2185fd545b3732c548d8c63af6338598cb19eb1
Karolkzsp5 <kondrackikarol688@gmail.com>
2025-04-08 18:12:36 +0200

    Zadania lab_4 i 5

---
+++ Lab_5/Zadanie_9.1.py
@@ -0,0 +1,5 @@
+inw = ("Litwo! Ojczyzno moja! ty jesteś jak zdrowie.\n"
+       "Ile cię trzeba cenić, ten tylko się dowie,\n"
+       "Kto cię stracił. Dziś piękność twą w całej ozdobie\n"
+       "Widzę i opisuję, bo tęsknię po tobie.")
+print(inw)
